c96423b0a2490f58099c3664245e74194788166e
Russell McLaughlin <russellray16@gmail.com>
2018-04-06 15:18:27 -0500

    Edited product-list components

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { IProduct } from './product'
 
 @Component ({
     selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
 
 export class ProductListComponent {
@@ -11,7 +13,7 @@ export class ProductListComponent {
     imageMargin: number = 2;
     showImage: boolean = false;
     listFilter: string = "";
-    products: any[] = [
+    products: IProduct[] = [
         {
             "productId": 1,
             "productName": "Leaf Rake",
