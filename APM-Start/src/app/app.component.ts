ce9f90f311a0f0a5fcdb9252818fca56cafe509b
russell <russellray16@gmail.com>
2018-04-08 14:49:20 -0500

    Created product service and custom pipe

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { ProductListComponent } from './products/product-list.component';
+import { ProductService } from './products/product.service';
 
-@Component({
+@Component ({
+
   selector: 'pm-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ProductService]
+
 })
+
 export class AppComponent {
   title = 'Product Management';
 }
